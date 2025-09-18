abb29541ae2f81355745cc742ed656884bae9172
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-18 12:27:34 +0430

    modify user route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import userRoutes from "./routes/user.route.js";
 
 const app = express();
+app.use(express.json());
 
 app.use("users", userRoutes);
 
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,6 +4,7 @@ import { getUsers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
+// router.get('/', authenticate, authorize('admin', 'manager'), getUsers);
 router.get("/", getUsers);
 
 export default router;
