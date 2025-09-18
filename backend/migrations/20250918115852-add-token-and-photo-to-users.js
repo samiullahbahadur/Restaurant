b8d5cb3ef500a20c86414e9637f54a5888a5b3a6
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-18 16:34:14 +0430

    add new column in user model

--- backend/migrations/20250918115852-add-token-and-photo-to-users.js
+++ backend/migrations/20250918115852-add-token-and-photo-to-users.js
@@ -1,17 +1,20 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 export default {
-  async up (queryInterface, Sequelize) {
-   
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Users", "token", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+    await queryInterface.addColumn("Users", "photo", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Users", "token");
+    await queryInterface.removeColumn("Users", "photo");
+  },
 };
