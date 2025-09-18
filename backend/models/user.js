1284a71bb88e99f6260b0ef4382de53095d858a7
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-18 12:06:06 +0430

    added jsonwebtoken  and change the user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,27 +1,59 @@
+// "use strict";
+
 "use strict";
-import Model from "sequelize";
+import { Model } from "sequelize";
+import bcrypt from "bcrypt";
+
 export default (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // Add associations here if needed
+    }
+
+    // Check password
+    async validPassword(password) {
+      return await bcrypt.compare(password, this.password);
     }
   }
   User.init(
     {
-      firstname: DataTypes.STRING,
-      lastname: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      role: DataTypes.STRING,
+      firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.ENUM("customer", "waiter", "chef", "manager", "admin"),
+        allowNull: false,
+        defaultValue: "customer",
+      },
     },
     {
       sequelize,
       modelName: "User",
+      hooks: {
+        beforeCreate: async (user) => {
+          user.password = await bcrypt.hash(user.password, 10);
+        },
+        beforeUpdate: async (user) => {
+          if (user.changed("password")) {
+            user.password = await bcrypt.hash(user.password, 10);
+          }
+        },
+      },
     }
   );
   return User;
