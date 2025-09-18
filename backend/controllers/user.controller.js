d8eb7be2d2d9a331ffc1d7b05d3e774d63030e41
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-18 12:23:39 +0430

    modify user controller getuser and added getuserbyId

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,12 +1,30 @@
 import User from "../models/user.js";
 
-export const getUsers = (req, res) => {
-  const user = User.findAll();
-  res.status(200).json(
-    {
-      success: true,
-      message: " Successfull find users",
-    },
-    user
-  );
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({
+      attributes: { exclude: ["password"] }, // hide password
+    });
+    res.status(200).json({
+      succss: true,
+      message: "found users",
+      users,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      attributes: { exclude: ["password"] },
+    });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 };
