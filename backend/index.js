a68795b0358e12b7afab85be8ad5d6e8e422e15b
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-18 17:03:42 +0430

    added multer to index file

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import userRoutes from "./routes/user.route.js";
 const app = express();
 app.use(express.json());
 
+app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
+
 app.use("/users", userRoutes);
 
 app.listen(5000, () => {
