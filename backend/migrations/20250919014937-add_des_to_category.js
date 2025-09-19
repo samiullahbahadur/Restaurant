254bef2817ed3656be1e7be60ad97360ff9c7c42
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-19 06:23:25 +0430

    modified new migration

--- backend/migrations/20250919014937-add_des_to_category.js
+++ backend/migrations/20250919014937-add_des_to_category.js
@@ -1,12 +1,15 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 export default {
-  async up (queryInterface, Sequelize) {
-    
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Category", "description", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-   
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn("Category","description")
+  },
 };
