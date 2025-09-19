3ff10aa83fbac99c9bb618c0b62a54ce1ea2a6dd
samiullahbahadur <bahadursamiullah@gmail.com>
2025-09-19 06:20:30 +0430

    created new migration

---
+++ backend/migrations/20250919014937-add_des_to_category.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up (queryInterface, Sequelize) {
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+   
+  }
+};
